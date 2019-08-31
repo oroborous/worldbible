ee8c899cee2142b7875a3b04732f31f4b7eda08d
Stacy Read <stacy.read.007@gmail.com>
2019-08-31 13:13:27 -0500

    Changed to IntelliJ project

---
+++ src/DatabaseTestServlet.java
@@ -0,0 +1,36 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.*;
+
+@WebServlet(name = "DatabaseTestServlet", urlPatterns = "/db")
+public class DatabaseTestServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:derby:memory");
+            Statement stmt = conn.createStatement();
+            ResultSet rset = stmt.executeQuery("SELECT * FROM category");
+
+            StringBuilder sb = new StringBuilder();
+            sb.append("<html><body><ul>");
+            while (rset.next()) {
+                String name = rset.getString(1);
+                sb.append("<li>").append(name).append("</li>");
+            }
+            sb.append("</ul></body></html>");
+            stmt.close();
+            conn.close();
+
+            response.getWriter().print(sb.toString());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
