5c8702241133563afb90184be814dd565834f00d
Stacy Read <stacy.read.007@gmail.com>
2019-09-02 23:55:07 -0500

    Changed back to try-catch-finally

--- src/DatabaseTestServlet.java
+++ src/DatabaseTestServlet.java
@@ -19,6 +19,11 @@ public class DatabaseTestServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Declare outside the try/catch so the variables are in scope in the finally block
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rset = null;
+
         // Load the driver
         try {
             Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
@@ -26,40 +31,38 @@ public class DatabaseTestServlet extends HttpServlet {
             // Find the absolute path of the database folder
             String absPath = getServletContext().getRealPath(DATABASE_PATH);
 
-            try (
-                    // Create a connection
-                    Connection conn = DriverManager.getConnection(DRIVER_NAME + absPath, USERNAME, PASSWORD);
-                    // Create a statement to execute SQL
-                    Statement stmt = conn.createStatement();
-                    // Execute a SELECT query and get a result set
-                    ResultSet rset = stmt.executeQuery("SELECT * FROM category");
-            ) {
-                // Create a StringBuilder for ease of appending strings
-                StringBuilder sb = new StringBuilder();
-                // HTML to create a simple web page
-                sb.append("<html><body><ul>");
+            // Create a connection
+            conn = DriverManager.getConnection(DRIVER_NAME + absPath, USERNAME, PASSWORD);
+            // Create a statement to execute SQL
+            stmt = conn.createStatement();
+            // Execute a SELECT query and get a result set
+            rset = stmt.executeQuery("SELECT * FROM category");
 
-                // Loop while the result set has more rows
-                while (rset.next()) {
-                    // Get the first string from each record
-                    String name = rset.getString("category_nm");
-                    // Append it as a list item
-                    sb.append("<li>").append(name).append("</li>");
-                }
-                // Close all those opening tags
-                sb.append("</ul></body></html>");
+            // Create a StringBuilder for ease of appending strings
+            StringBuilder output = new StringBuilder();
+            // HTML to create a simple web page
+            output.append("<html><body><ul>");
 
-                // Send the HTML as the response
-                response.getWriter().print(sb.toString());
-            } catch (SQLException e) {
-                // If there's an exception closing any resources, send IT as the response
-                response.getWriter().print(e.getMessage());
-                e.printStackTrace();
+            // Loop while the result set has more rows
+            while (rset.next()) {
+                // Get the first string from each record
+                String name = rset.getString("category_nm");
+                // Append it as a list item
+                output.append("<li>").append(name).append("</li>");
             }
-        } catch (ClassNotFoundException e) {
+            // Close all those opening tags
+            output.append("</ul></body></html>");
+
+            // Send the HTML as the response
+            response.getWriter().print(output.toString());
+
+        } catch (SQLException | ClassNotFoundException e) {
             // If there's an exception locating the driver, send IT as the response
             response.getWriter().print(e.getMessage());
+            // Print stack trace
             e.printStackTrace();
+            // Close database objects
+            DatabaseUtils.closeAll(conn, stmt, rset);
         }
     }
 }
---
+++ src/DatabaseUtils.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseUtils {
+    public static void closeAll(Connection conn, Statement stmt, ResultSet rset) {
+        if (rset != null) {
+            try {
+                rset.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
